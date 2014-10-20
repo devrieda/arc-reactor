69a63d7f8134c1f048faf89ddcc5c348583e6246
Derek DeVries <devrieda@gmail.com>
2014-10-20 09:24:47 -0600

    add stubs for tests for all modules/components

---
+++ __tests__/modules/KeyIntent-test.js
@@ -0,0 +1,8 @@
+jest.dontMock('../../app/modules/KeyIntent');
+
+describe('KeyIntent', function() {
+  it('translate a key press event', function() {
+    var KeyIntent = require('../../app/modules/KeyIntent');
+  });
+});
+
---
+++ __tests__/modules/Selection-test.js
@@ -0,0 +1,9 @@
+jest.dontMock('../../app/modules/Selection');
+
+describe('Selection', function() {
+  it('create a new selection', function() {
+    var Selection = require('../../app/modules/Selection');
+  });
+});
+
+

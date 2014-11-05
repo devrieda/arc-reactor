c22a47f15396e5d89f473564d3c9449ef0d2c78f
Derek DeVries <devrieda@gmail.com>
2014-11-04 22:59:59 -0700

    cleanup of selection, find cumulative position in selection in respect to block

--- app/modules/Selection.js
+++ app/modules/Selection.js
@@ -9,6 +9,9 @@
 //   anchorOffset: 2
 //   focusOffset: 10
 //
+//   anchorBlockOffset: 10
+//   focusBlockOffset: 20
+//
 class Selection {
   constructor(selection) {
     this.selection = selection;
@@ -20,21 +23,21 @@ class Selection {
     }
   }
 
+  // reselect text if we've swapped out the dom underneath
   reselect() {
     if (!this.anchorGuid || !this.focusGuid) { return false; }
-
-    // do we need to reselect?
     if (this.selection.type == 'Range') { return this._initBounds(); }
 
     // set the range based on selection node state
     var startNode = this._anchorTextNode();
     var endNode   = this._focusTextNode();
-    var startOffset = this.anchorOffset;
-    var endOffset   = this.focusOffset;
+    var startOffset = this.anchorBlockOffset;
+    var endOffset   = this.focusBlockOffset;
 
+    if (!startNode || !endNode) { return; }
+
+    // set range, swap if user selected back to front
     this._setRange(startNode, endNode, startOffset, endOffset);
-
-    // user selected from back to front
     if (this.selection.getRangeAt(0).collapsed) {
       this._setRange(endNode, startNode, endOffset, startOffset);
     }
@@ -48,6 +51,7 @@ class Selection {
     this.selection.addRange(range);
   }
 
+  // set selection focus on a specific block by guid
   focusOn(guid, offset) {
     this.anchorGuid = guid;
     this.focusGuid  = guid;
@@ -57,7 +61,7 @@ class Selection {
   }
 
 
-  // find position of selection
+  // find general positions of selection
   isRange() {
     return this.selection.type == "Range";
   }
@@ -82,15 +86,118 @@ class Selection {
   }
 
 
-  // the selection has an anchor and is within the content
-  _isValid() {
-    var node = this._anchorNode()
-    if (!node) { return false; }
+  // cache the node offsets, but also the total offset within the block text
+  _initNodes() {
+    this.anchorOffset = this.selection.anchorOffset;
+    this.focusOffset  = this.selection.focusOffset;
+    this.anchorGuid   = this._anchorBlock().getAttribute('name');
+    this.focusGuid    = this._focusBlock().getAttribute('name');
+    this.anchorBlockOffset = this._anchorBlockOffset();
+    this.focusBlockOffset  = this._focusBlockOffset();
+  }
+  _anchorBlockOffset() {
+    return this._nodeOffset(
+      this._anchorBlock(),
+      this.selection.anchorNode,
+      this.selection.anchorOffset
+    );
+  }
+  _focusBlockOffset() {
+    return this._nodeOffset(
+      this._focusBlock(),
+      this.selection.focusNode,
+      this.selection.focusOffset
+    );
+  }
+  _nodeOffset(block, node, offset) {
+    var nodes = this._textNodes(block);
+    var total = 0;
+    for (var i = 0, j = nodes.length; i < j; i++) {
+      if (nodes[i] == node) {
+        total += offset;
+        break;
+      } else {
+        total += nodes[i].length;
+      }
+    }
+    return total;
+  }
+  _textNodes(node, textNodes) {
+    var textNodes = textNodes || [];
+    var children = node.childNodes;
 
-    while (node.tagName && !node.getAttribute('contenteditable')) {
+    for (var i = 0, j = children.length; i < j; i++) {
+      var child = children[i];
+      if (child.nodeType == Node.TEXT_NODE) {
+        textNodes.push(child);
+      } else {
+        this._textNodes(child, textNodes);
+      }
+    }
+    return textNodes;
+  }
+
+  // find parent block nodes
+  _anchorBlock() {
+    var node = this._anchorNode();
+    while (!node.classList.contains("ic-Editor-Block")) {
       node = node.parentNode;
     }
-    return node !== document;
+    return node;
+  }
+  _focusBlock() {
+    var node = this._focusNode();
+    while (!node.classList.contains("ic-Editor-Block")) {
+      node = node.parentNode;
+    }
+    return node;
+  }
+  // closest non-text node
+  _anchorNode() {
+    var node = this.selection.anchorNode;
+    return node && node.nodeType === 3 ? node.parentNode : node;
+  }
+  _focusNode() {
+    var node = this.selection.focusNode;
+    return node && node.nodeType === 3 ? node.parentNode : node;
+  }
+
+  // selection bounds
+  _initBounds() {
+    var old = this.bounds;
+    this.bounds = this._bounds();
+    return JSON.stringify(old) != JSON.stringify(this.bounds);
+  }
+  _bounds() {
+    var range = this.selection.getRangeAt(0);
+    return range.getBoundingClientRect();
+  }
+
+  // reconstituting selection of node from attributes
+  _anchorTextNode() {
+    var block = this._blockNode(this.anchorGuid);
+    return this._findChild(block, this.anchorOffset)
+  }
+  _focusTextNode() {
+    var block = this._blockNode(this.focusGuid);
+    return this._findChild(block, this.focusOffset)
+  }
+  _blockNode(guid) {
+    return document.getElementsByClassName(`ic-Editor-Block--${guid}`)[0];
+  }
+  _findChild(block, offset, pos) {
+    var pos = pos || 0;
+    var children = block.childNodes;
+
+    for (var i = 0, j = children.length; i < j; i++) {
+      var child = children[i];
+      if (child.nodeType == Node.TEXT_NODE) {
+        pos += child.length;
+        if (offset < pos) { return child; }
+      } else {
+        return this._findChild(child, offset, pos);
+      }
+    }
   }
 
   // initializers
@@ -119,90 +226,15 @@ class Selection {
     return node.getAttribute('data-align') == "center";
   }
 
+  // the selection has an anchor and is within the content
+  _isValid() {
+    var node = this._anchorNode()
+    if (!node) { return false; }
 
-  _initNodes() {
-    this.anchorGuid   = this._anchorGuid();
-    this.focusGuid    = this._focusGuid();
-    this.anchorOffset = this.selection.anchorOffset;
-    this.focusOffset  = this.selection.focusOffset;
-  }
-  _anchorGuid() {
-    return this._anchorBlock().getAttribute('name');
-  }
-  _focusGuid() {
-    return this._focusBlock().getAttribute('name');
-  }
-  _anchorOffset() {
-    var anchor = this.selection.anchorNode;
-  }
-  _focusOffset() {
-    var focus = this.selection.focusNode;
-  }
-
-
-  // go up all the way to the block
-  _anchorBlock() {
-    var node = this._anchorNode();
-    while (!node.classList.contains("ic-Editor-Block")) {
+    while (node.tagName && !node.getAttribute('contenteditable')) {
       node = node.parentNode;
     }
-    return node;
-  }
-  _focusBlock() {
-    var node = this._focusNode();
-    while (!node.classList.contains("ic-Editor-Block")) {
-      node = node.parentNode;
-    }
-    return node;
-  }
-  // get the node (either inline or block)
-  _anchorNode() {
-    var node = this.selection.anchorNode;
-    return node && node.nodeType === 3 ? node.parentNode : node;
-  }
-  _focusNode() {
-    var node = this.selection.focusNode;
-    return node && node.nodeType === 3 ? node.parentNode : node;
-  }
-
-
-  // selection bounds
-  _initBounds() {
-    var old = this.bounds;
-    this.bounds = this._bounds();
-
-    return JSON.stringify(old) != JSON.stringify(this.bounds);
-  }
-  _bounds() {
-    var range = this.selection.getRangeAt(0);
-    return range.getBoundingClientRect();
-  }
-
-
-  // get the text
-  _anchorTextNode() {
-    return this._textNode(this.anchorGuid, this.anchorOffset);
-  }
-  _focusTextNode() {
-    return this._textNode(this.focusGuid, this.focusOffset);
-  }
-  _textNode(guid, offset) {
-    var block = document.getElementsByClassName(`ic-Editor-Block--${guid}`)[0];
-    return this._findChild(block, offset)
-  }
-  _findChild(block, offset, pos) {
-    var pos = pos || 0;
-    var children = block.childNodes;
-
-    for (var i = 0, j = children.length; i < j; i++) {
-      var node = children[i];
-      if (node.nodeType == Node.TEXT_NODE) {
-        pos += node.length;
-        if (offset < pos) { return node; }
-      } else {
-        return this._findChild(node, offset, pos);
-      }
-    }
+    return node !== document;
   }
 }
 

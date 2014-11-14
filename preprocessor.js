33c013413bdfa6b6fe54bb2f628f98d41ac3e4e4
Derek DeVries <devrieda@gmail.com>
2014-11-13 18:42:07 -0700

    use harmony for tests and skip scss files

--- preprocessor.js
+++ preprocessor.js
@@ -2,7 +2,7 @@ var ReactTools = require('react-tools');
 
 module.exports = {
   process: function(src, path) {
-    if (path.match(/\.css$/)) { return ''; }
-    return ReactTools.transform(src);
+    if (path.match(/css$/)) { return ''; }
+    return ReactTools.transform(src, {harmony: true});
   }
 };

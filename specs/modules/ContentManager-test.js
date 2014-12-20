9089f08d7ef2396a9de1e23c1c63b04b23f76424
Derek DeVries <devrieda@gmail.com>
2014-12-20 14:05:34 -0700

    make block formatting work across multiple blocks

--- specs/modules/ContentManager-test.js
+++ specs/modules/ContentManager-test.js
@@ -459,7 +459,103 @@ describe('ContentManager', () => {
   })
 
   describe('#toggleBlockType', () => {
+    it('should add a block type for a single block', () => {
+      var block = {
+        "id": "c6a8",
+        "type": "p",
+        "text": "a"
+      }
+      content.sections[0].blocks = [block];
+      var manager = new ContentManager(content);
 
+      var guids = { anchor: 'c6a8', focus: 'c6a8' };
+      var result = manager.toggleBlockType(guids, 'h1');
+
+      var blocks = content.sections[0].blocks;
+      expect(blocks[0].type).toBe('h1');
+    })
+
+    it('should add a block type across multiple blocks', () => {
+      var block1 = {
+        "id": "c6a8",
+        "type": "p",
+        "text": "a"
+      }
+      var block2 = {
+        "id": "c6a9",
+        "type": "p",
+        "text": "b"
+      }
+      content.sections[0].blocks = [block1, block2];
+      var manager = new ContentManager(content);
+
+      var guids = { anchor: 'c6a8', focus: 'c6a9' };
+      var result = manager.toggleBlockType(guids, 'h1');
+
+      var blocks = content.sections[0].blocks;
+      expect(blocks[0].type).toBe('h1');
+      expect(blocks[1].type).toBe('h1');
+    })
+
+    it('should remove block type for a single block', () => {
+      var block = {
+        "id": "c6a8",
+        "type": "h1",
+        "text": "a"
+      }
+      content.sections[0].blocks = [block];
+      var manager = new ContentManager(content);
+
+      var guids = { anchor: 'c6a8', focus: 'c6a8' };
+      var result = manager.toggleBlockType(guids, 'h1');
+
+      var blocks = content.sections[0].blocks;
+      expect(blocks[0].type).toBe('p');
+    })
+
+    it('should remove block type across multiple blocks', () => {
+      var block1 = {
+        "id": "c6a8",
+        "type": "h1",
+        "text": "a"
+      }
+      var block2 = {
+        "id": "c6a9",
+        "type": "h1",
+        "text": "b"
+      }
+      content.sections[0].blocks = [block1, block2];
+      var manager = new ContentManager(content);
+
+      var guids = { anchor: 'c6a8', focus: 'c6a9' };
+      var result = manager.toggleBlockType(guids, 'h1');
+
+      var blocks = content.sections[0].blocks;
+      expect(blocks[0].type).toBe('p');
+      expect(blocks[1].type).toBe('p');
+    })
+
+    it('should add block type to all blocks if a single one isnt formatted', () => {
+      var block1 = {
+        "id": "c6a8",
+        "type": "h1",
+        "text": "a"
+      }
+      var block2 = {
+        "id": "c6a9",
+        "type": "h1",
+        "text": "b"
+      }
+      content.sections[0].blocks = [block1, block2];
+      var manager = new ContentManager(content);
+
+      var guids = { anchor: 'c6a8', focus: 'c6a9' };
+      var result = manager.toggleBlockType(guids, 'h1');
+
+      var blocks = content.sections[0].blocks;
+      expect(blocks[0].type).toBe('p');
+      expect(blocks[1].type).toBe('p');
+    })
   })
 
   describe('#toggleMarkup', () => {

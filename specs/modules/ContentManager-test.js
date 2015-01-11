a1753e20f0967a0fa94d3c7c60c0379ceabf1445
Derek DeVries <devrieda@gmail.com>
2015-01-11 15:58:44 -0700

    first step in toggling multi-block markup

--- specs/modules/ContentManager-test.js
+++ specs/modules/ContentManager-test.js
@@ -586,28 +586,53 @@ describe('ContentManager', () => {
     })
 
     it('should add markup across multiple blocks', () => {
+      var block1 = {
+        "id": "c6a7",
+        "type": "p",
+        "text": "You can imagine where it goes from here."
+      }
+      var block2 = {
+        "id": "c6a8",
+        "type": "p",
+        "text": "He fixes the cable?"
+      }
+      var block3 = {
+        "id": "c6a9",
+        "type": "p",
+        "text": "Don't be fatuous, Jeffrey."
+      }
 
+      content.sections[0].blocks = [block1, block2, block3];
+      var manager = new ContentManager(content);
+
+      var guids = { anchor: 'c6a7', focus: 'c6a9' };
+      var offsets = { anchor: 2, focus: 4 };
+      var result = manager.toggleMarkup(guids, offsets, 'strong');
+
+      expect(block1.markups).toEqual({"strong":[{"range": [2,40]}]});
+      expect(block2.markups).toEqual({"strong":[{"range": [0,19]}]});
+      expect(block3.markups).toEqual({"strong":[{"range": [0,4]}]});
     })
 
     it('should remove markup for a single block', () => {
-      // var block = {
-      //   "id": "c6a8",
-      //   "type": "p",
-      //   "text": "this is my formatted text",
-      //   "markups": {
-      //     "strong": [{
-      //       "range": [0,4]
-      //     }]
-      //   }
-      // }
-      // content.sections[0].blocks = [block];
-      // var manager = new ContentManager(content);
+      var block = {
+        "id": "c6a8",
+        "type": "p",
+        "text": "this is my formatted text",
+        "markups": {
+          "strong": [{
+            "range": [0,4]
+          }]
+        }
+      }
+      content.sections[0].blocks = [block];
+      var manager = new ContentManager(content);
 
-      // var guids   = { anchor: 'c6a8', focus: 'c6a8' };
-      // var offsets = { anchor: 0, focus: 4 };
-      // var result = manager.toggleMarkup(guids, offsets, 'strong');
+      var guids   = { anchor: 'c6a8', focus: 'c6a8' };
+      var offsets = { anchor: 0, focus: 4 };
+      var result = manager.toggleMarkup(guids, offsets, 'strong');
 
-      // expect(block.markups).toEqual({"strong":[]});
+      expect(block.markups).toEqual({"strong":[]});
     })
 
     it('should remove markup across multiple blocks', () => {
